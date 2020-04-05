eaec682059161c7ac4d875411225ff8294a507f2
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-04-05 12:56:54 +0200

    fetching the json from wp

---
+++ main.js
@@ -0,0 +1,23 @@
+fetch("http://andreimihutoni.com/wp_bolig/wp-json/wp/v2/property")
+    .then(res => res.json())
+    .then(handleData)
+
+function handleData(property) {
+    property.forEach(showPost)
+
+
+}
+
+function showPost(property) {
+    console.log(property)
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+
+
+    copy.querySelector("h2").innerHTML = property.title.rendered;
+    copy.querySelector(".body").innerHTML = property.content.rendered;
+
+
+
+    document.querySelector("main").appendChild(copy);
+}
